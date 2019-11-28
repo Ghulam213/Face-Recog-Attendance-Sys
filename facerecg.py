aed69c0a2531b9e5cf1ba29be63ef2bbf0ad4e02
OsamaAkhtar <57761675+OsamaAkhtar@users.noreply.github.com>
2019-11-28 10:41:13 +0500

    Face recg

--- facerecg.py
+++ facerecg.py
@@ -1,48 +1,31 @@
 import face_recognition
-import cv2
-from functions import student_name
+import time
+import pickle
 
-import cv2
 
-cam = cv2.VideoCapture(0)
+start = time.time()
 
-cv2.namedWindow("test")
+infile = open('D:\Documents\FRAttendanceSystem\\pickled_encodings','rb')
+new_encodings = pickle.load(infile)
+infile.close()
 
-img_counter = 0
+def student_encodings(name):
+    image = face_recognition.load_image_file('D:\Documents\FRAttendanceSystem\images\{}.jpg.'.format(name))
+    image_encodings = face_recognition.face_encodings(image)[0]
+    return face_encodings
 
-while True:
-    ret, frame = cam.read()
-    cv2.imshow("test", frame)
-    if not ret:
+
+for encoding in pickled_encodings:
+    student = student_encodings('Abdullah Zubair')
+    result = face_recognition.compare_faces([student], pickled_encodings)
+    if result[0]:
+        print('Present in class!')
         break
-    k = cv2.waitKey(1)
-
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-
-cam.release()
-
-cv2.destroyAllWindows()
-
-face = face_recognition.load_image_file('D:\Documents\FRAttendanceSystem\{}'.format(img_name))
-face_encodings = face_recognition.face_encodings(img_name)[0]
-
-rakshak = face_recognition.load_image_file('D:\Documents\FRAttendanceSystem\images\Raja Rakshak.jpg')
-encodings = face_recognition.face_encodings(rakshak)[0]
-
-
-results = face_recognition.compare_faces([face_encodings], encodings)
-
-if results[0]:
-    print('ok')
+    else:
+        continue
 else:
-    print('okkkk')
+    print('Absent!')
 
+end = time.time()
+
+print('It took {}s for the code to execute!'.format(end-start))
