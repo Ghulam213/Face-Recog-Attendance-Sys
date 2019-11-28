60ea68efed24103aea60cbb3858971ac19549a5d
OsamaAkhtar <57761675+OsamaAkhtar@users.noreply.github.com>
2019-11-28 15:19:01 +0500

    Pickled

--- facerecg.py
+++ facerecg.py
@@ -1,31 +1,60 @@
 import face_recognition
+import cv2
 import time
 import pickle
+import concurrent.futures
 
 
 start = time.time()
 
-infile = open('D:\Documents\FRAttendanceSystem\\pickled_encodings','rb')
-new_encodings = pickle.load(infile)
-infile.close()
+cam = cv2.VideoCapture(0)
 
-def student_encodings(name):
-    image = face_recognition.load_image_file('D:\Documents\FRAttendanceSystem\images\{}.jpg.'.format(name))
-    image_encodings = face_recognition.face_encodings(image)[0]
-    return face_encodings
+cv2.namedWindow("test")
 
+img_counter = 0
 
-for encoding in pickled_encodings:
-    student = student_encodings('Abdullah Zubair')
-    result = face_recognition.compare_faces([student], pickled_encodings)
-    if result[0]:
-        print('Present in class!')
+while True:
+    ret, frame = cam.read()
+    cv2.imshow("test", frame)
+    if not ret:
         break
-    else:
-        continue
+    k = cv2.waitKey(1)
+
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "{}.jpg".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+
+
+cam.release()
+
+cv2.destroyAllWindows()
+
+webcam_image = face_recognition.load_image_file('D:\Documents\FRAttendanceSystem\{}'.format(img_name))
+webcam_encodings = face_recognition.face_encodings(webcam_image)[0]
+
+
+abdzubair = face_recognition.load_image_file('D:\Documents\FRAttendanceSystem\images\Abdullah Zubair.jpg')
+encodingszubair = face_recognition.face_encodings(abdzubair)[0]
+
+with open('pickled_encodings2.pkl', 'rb') as pickle_file:
+    new_encodings = pickle.load(pickle_file)
+
+for encoding in new_encodings:
+    result = face_recognition.compare_faces([encodingszubair], encoding)
+    if result[0]:
+        print('Present!')
+        break
 else:
     print('Absent!')
 
 end = time.time()
+
+
 
 print('It took {}s for the code to execute!'.format(end-start))
